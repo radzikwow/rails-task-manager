ee2328ed5bf2fca54b9c3ffd8ce7df18560e3cc2
Filip Radzikowski <radzikpl@hotmail.com>
2022-05-24 16:52:26 +0200

    implemented index, show and new functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get "index", to: "tasks#index"
+  get ":id", to: "tasks#show", as: :task
+  get "new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get ":id/edit", to: "tasks#edit", as: :edit_task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
